9df20f80881dc9bfcdcac669457a6b9f54e24ca0
Maruf <mamaruf317@gmail.com>
2025-07-29 16:04:29 +0600

    Refactor formatting of Reverse Integer solution for consistency

--- 7.reverse-integer.cpp
+++ 7.reverse-integer.cpp
@@ -5,11 +5,18 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int reverse(int x) {
-        
+    int reverse(int x)
+    {
+        long long rev = 0;
+        while (x != 0)
+        {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return (int)rev;
     }
 };
 // @lc code=end
-
