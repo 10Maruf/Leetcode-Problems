0fe6645a13ae56a0ce6e9e091c0e9e847704e3f6
Maruf <mamaruf317@gmail.com>
2025-07-29 16:00:24 +0600

    Refactor Zigzag Conversion solution and add initial implementation for Reverse Integer problem

--- 6.zigzag-conversion.cpp
+++ 6.zigzag-conversion.cpp
@@ -5,11 +5,30 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    string convert(string s, int numRows) {
-        
+    string convert(string s, int numRows)
+    {
+        if (numRows == 1 || s.size() <= numRows)
+            return s;
+        vector<vector<char>> zigzag(numRows);
+        int row = 0, step = 1;
+        for (char c : s)
+        {
+            zigzag[row].push_back(c);
+            if (row == 0)
+                step = 1;
+            else if (row == numRows - 1)
+                step = -1;
+            row += step;
+        }
+        string result;
+        for (const auto &r : zigzag)
+        {
+            result.append(r.begin(), r.end());
+        }
+        return result;
     }
 };
 // @lc code=end
-
