c7133157e7407f29134ba790040f257167c0275c
Maruf <mamaruf317@gmail.com>
2025-08-01 03:11:59 +0600

    fix reverse integer problem

--- 7.reverse-integer.cpp
+++ 7.reverse-integer.cpp
@@ -13,8 +13,8 @@ public:
         int rev = 0;
         while (x != 0)
         {
-            rev = rev * 10 + x % 10;
             if (rev > INT_MAX / 10 || rev < INT_MIN / 10) return 0;
+            rev = rev * 10 + x % 10;
             x /= 10;
         }
         return rev;
