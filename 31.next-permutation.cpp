a427e35f7b5c2819cd7128f91e9ff238d988932f
Maruf <mamaruf317@gmail.com>
2025-08-26 15:06:18 +0600

    feat: replace standard library next_permutation with custom implementation

--- 31.next-permutation.cpp
+++ 31.next-permutation.cpp
@@ -8,9 +8,20 @@
 class Solution {
 public:
     vector<int> nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = n - 1;
+            while (j > i && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+        reverse(nums.begin() + i + 1, nums.end());
         return nums;
-
     }
 };
 // @lc code=end
