cb7fb95cf9637392e0986dcf4c6d26e480042a96
Maruf <mamaruf317@gmail.com>
2025-09-02 11:03:22 +0600

    feat: add solutions for Remove All Adjacent Duplicates In String, Length of Last Word, and Backspace String Compare

---
+++ 58.length-of-last-word.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=58 lang=cpp
+ *
+ * [58] Length of Last Word
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        for (int i = s.length()-1; i >= 0; i--)
+        {
+            if (s[i] == ' ' && count > 0)
+                return count;
+            if (s[i] != ' ')
+                count++;
+        }
+        return count;
+    }
+};
+// @lc code=end
+
