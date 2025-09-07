c333ba9a73c1bae9ce7d3d1addaf9cbaf37bb11a
Maruf <mamaruf317@gmail.com>
2025-09-07 20:51:43 +0600

    feat: implement Next Greater Element II solution in C++

---
+++ 503.next-greater-element-ii.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=503 lang=cpp
+ *
+ * [503] Next Greater Element II
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> nextGreaterElements(vector<int> &nums)
+    {
+        stack<int> st;
+        int n = nums.size();
+        vector<int> res(n, -1);
+        for (int i = 0; i < 2 * n; ++i)
+        {
+            while (!st.empty() && nums[st.top()] < nums[i % n])
+            {
+                res[st.top()] = nums[i % n];
+                st.pop();
+            }
+            if (i < n)
+                st.push(i);
+        }
+        return res;
+    }
+};
+// @lc code=end
